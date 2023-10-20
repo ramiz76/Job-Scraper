5c85b1c0c6ab497b4d50cae0f8c1c1968b4c4595
ramiz <ramizalipir76@gmail.com>
2023-10-20 12:42:21 +0100

    Created new test functions for extract.py.

--- pipeline/test_extract.py
+++ pipeline/test_extract.py
@@ -1,7 +1,8 @@
 """Unit tests for extract.py"""
+import pytest
 
-from conftest import FakeListingURL, FakeDriver, FakeWebpageHref, FakeWebpageNoHref
-from extract import make_listings_request, get_webpages_href, get_job_id
+from conftest import FakeListingURL, FakeDriver, FakeWebpageHref, FakeWebpageNoHref, FakeWebpageListings
+from extract import make_listings_request, get_webpages_href, get_job_id, get_listings_href
 
 
 def test_successful_webpage_request_returns_string():
@@ -32,19 +33,24 @@ def test_get_webpages_returns_None_if_no_additional_website_pages():
     assert not response
 
 
-# Use parameters for this test
-def test_job_id_retrieved_from_listing_href():
-    fake_href = "https://www.fake_listing-job101156323"
+@pytest.mark.parametrize("valid_job_id", [("job101156323"), ("job01153433"), ("job323"), ("job1")])
+def test_job_id_retrieved_from_listing_href(valid_job_id):
+    fake_href = f"https://www.fake_listing-{valid_job_id}"
     job_id = get_job_id(fake_href)
-    assert job_id.group() == "job101156323"
+    assert job_id.group() == valid_job_id
 
 
-# Use parameters for this test
-def test_no_job_id_in_listing_href_returns_None():
-    fake_href = "https://www.fake_listing-101156323"
+@pytest.mark.parametrize("invalid_job_id", [("j10113"), ("job-03"), ("323"), ("job")])
+def test_no_job_id_in_listing_href_returns_None(invalid_job_id):
+    fake_href = f"https://www.fake_listing-{invalid_job_id}"
     job_id = get_job_id(fake_href)
     assert job_id == None
 
 
-def test_get_listings_href():
-    ""
+@pytest.mark.parametrize("expected_href", [("fake_href"), ("/job/data-engineer/develop-job101378062"), ("sfjksfje23!!@342|")])
+def test_get_listings_href(expected_href):
+    fake_html = FakeWebpageListings(expected_href)
+    listings_href = get_listings_href(fake_html)
+    print(listings_href)
+    assert isinstance(listings_href, list)
+    assert all(href == expected_href for href in listings_href)
