4b99bba3b1cfcbf863af7fc94b57c823d78389ff
ramiz <ramizalipir76@gmail.com>
2023-10-20 12:44:20 +0100

    Added new fixtures for testing extract.py

--- pipeline/conftest.py
+++ pipeline/conftest.py
@@ -16,10 +16,12 @@ def FakeDriver():
 
 def FakeWebpageHref():
     html = """<li class="res-44kvrv" data-genesis-element="TEXT" role="listitem"><a class="res-1joyc6q" 
-    data-genesis-element="BUTTON" aria-label="2 of 3" href="https://www.totaljobs.com/jobs/data-engineer/in-london?radius=0&amp;page=2&amp;postedWithin=3" 
+    data-genesis-element="BUTTON" aria-label="2 of 3" 
+    href="https://www.totaljobs.com/jobs/data-engineer/in-london?radius=0&amp;page=2&amp;postedWithin=3" 
     aria-busy="false" aria-current="false"><span class="res-1cekje2" data-genesis-element="BASE"><span class="res-vurnku" 
     data-genesis-element="BASE"><span aria-hidden="true">2</span></span></span></a></li><
-    li class="res-44kvrv" data-genesis-element="TEXT" role="listitem"><a class="res-1joyc6q" data-genesis-element="BUTTON" aria-label="3 of 3" href="https://www.totaljobs.com/jobs/data-engineer/in-london?radius=0&amp;page=3&amp;postedWithin=3"
+    li class="res-44kvrv" data-genesis-element="TEXT" role="listitem"><a class="res-1joyc6q" data-genesis-element="BUTTON" 
+    aria-label="3 of 3" href="https://www.totaljobs.com/jobs/data-engineer/in-london?radius=0&amp;page=3&amp;postedWithin=3"
     aria-busy="false" aria-current="false"><span class="res-1cekje2" data-genesis-element="BASE">
     <span class="res-vurnku" data-genesis-element="BASE"><span aria-hidden="true">3</span></span></span></a></li>"""
     return BeautifulSoup(html, 'html.parser')
@@ -31,4 +33,16 @@ def FakeWebpageNoHref():
     href="https://www.totaljobs.com/jobs/data-engineer/in-bristol?radius=0&amp;page=1&amp;postedWithin=3" 
     aria-busy="false" aria-current="true"><span class="res-1cekje2" data-genesis-element="BASE">
     <span class="res-vurnku" data-genesis-element="BASE"><span aria-hidden="true">1</span></span></span></a></li>"""
+    return BeautifulSoup(html, 'html.parser')
+
+
+def FakeWebpageListings(href):
+    html = f"""<article class="res-1tps163" data-genesis-element="CARD" id="job-item-101378062" data-at="job-item">
+    <a class="res-1na8b7y" data-genesis-element="BASE" href={href} 
+    data-at="job-item-title" target="_self">
+</article><article class="res-1tps163" data-genesis-element="CARD" id="job-item-101378062" data-at="job-item">
+    <a class="res-1na8b7y" data-genesis-element="BASE" href={href} data-at="job-item-title" target="_self">
+</article><article class="res-1tps163" data-genesis-element="CARD" id="job-item-800062" data-at="job-item">
+    <a class="res-1na8b7y" data-genesis-element="BASE" href={href} data-at="job-item-title" target="_self">
+</article>"""
     return BeautifulSoup(html, 'html.parser')
