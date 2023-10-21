4102ce5e3b59429a0af31e9c512733537ec35cee
ramiz <ramizalipir76@gmail.com>
2023-10-21 19:45:57 +0100

    Practising skill extraction using Spacy PhraseMatcher

--- pipeline/transform.py
+++ pipeline/transform.py
@@ -2,6 +2,10 @@ import json
 
 from bs4 import BeautifulSoup
 import spacy
+from dotenv import load_dotenv
+from spacy.matcher import PhraseMatcher
+from spaczz.matcher import FuzzyMatcher
+
 
 # To extract:
 # Job posted date (if recently then todays date)
@@ -15,20 +19,20 @@ import spacy
 # benefits provided
 
 
-def open_html_file() -> str:
+def open_html_file() -> BeautifulSoup:
     """Retrieve HTML data from file and returns as BeautifulSoup object."""
     file = "london/listing/job101187548.html"
     file_two = "london/listing/job101266908.html"
-    with open(file_two) as html_file:
+    with open(file) as html_file:
         html = BeautifulSoup(html_file, 'html.parser')
     return html
 
 
-def parse_company_data(html: str) -> str:
+def parse_company_data(html: BeautifulSoup) -> str:
     """Extract data from listings full job description webpage"""
-    data = (html.find("script", id="jobPostingSchema")).string
-    data = json.loads(data)
-    return data
+    company_data = (html.find("script", id="jobPostingSchema")).string
+    company_data = json.loads(company_data)
+    return company_data
 
 
 def extract_company_data(data: str):
@@ -52,29 +56,49 @@ def extract_company_data(data: str):
 # may not be all of them
 # if not located then it will be bundled together within the <p> tags
 
-def extract_job_description(data: str):
+def extract_job_description(data: str) -> list:
+    """make this two functions"""
     desc = data.get('description')
     desc = BeautifulSoup(desc, 'html.parser')
-    all_text = desc.get_text()
-    # print(all_text)
 
     p_elements = desc.find_all('p')
     p_texts = [p.text for p in p_elements]
 
     li_elements = desc.find_all('li')
-    listed_text = [li.text for li in li_elements]
+    listed_desc = [li.text for li in li_elements]
 
-    return [p_texts, listed_text]
+    return listed_desc
 
 
-def extract_relevant_text(text: list, listed_text: list):
-    nlp_text = nlp(listed_text)
-    tokens = [token.text for token in nlp_text if not token.is_stop]
+def open_skills_json() -> set:
+    """Retrieve all skill names stored in json file as a set."""
+    with open('skills.json', 'r') as skills_json:
+        data = json.load(skills_json)
+    skills_set = set(data.keys())
+    return skills_set
+
+
+def extract_skills_from_description(listed_desc: list):
+    skills_list = []
+    for sentence in nlp.pipe(listed_desc, disable=["tok2vec", "tagger", "parser", "attribute_ruler", "lemmatizer"]):
+
+        matched_skills = skill_matcher(sentence)
+        for skill_dec in matched_skills:
+            skill = sentence[skill_dec[1]:skill_dec[2]]
+            skills_list.append(skill.text)
+    return skills_list
 
 
 if __name__ == "__main__":
+    load_dotenv()
     nlp = spacy.load("en_core_web_lg")
+    skills_set = open_skills_json()
+    # skill_matcher = FuzzyMatcher(nlp.vocab)
+    skill_matcher = PhraseMatcher(nlp.vocab)
+    patterns = set(nlp(text) for text in skills_set)
+    skill_matcher.add('SKILLS', None, *patterns)
+
     html = open_html_file()
-    data = parse_company_data(html)
-    desc = extract_job_description(data)
-    extract_relevant_text(desc[0], desc[1])
+    company_data = parse_company_data(html)
+    listed_desc = extract_job_description(company_data)
+    print(extract_skills_from_description(listed_desc))
