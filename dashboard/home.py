07b24c41b6a7f9c10f5d766cf5f890e8ff4e3793
ramiz <ramizalipir76@gmail.com>
2023-11-22 21:28:58 +0000

    Start dashboard using Streamlit.

---
+++ dashboard/home.py
@@ -0,0 +1,11 @@
+"""Homepage for dashboard"""
+import streamlit as st
+
+
+def dashboard_title():
+    st.title("Job Scraper")
+    st.markdown("placeholder")
+
+
+if __name__ == "__main__":
+    dashboard_title()
